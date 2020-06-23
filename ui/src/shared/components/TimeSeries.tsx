962df2a88e9bf506ad32a2ed37ee036565ccbcf7
asalem <ariel.salem1989@gmail.com>
2020-06-23 14:39:56 -0700

    feat(updateTimeRangeVEO): introduces an ephemeral listener for timeRange toggles within the VEO. This allows us to open and close a VEO without reloading the queries if the timeRange wasn't changed

--- ui/src/shared/components/TimeSeries.tsx
+++ ui/src/shared/components/TimeSeries.tsx
@@ -45,7 +45,7 @@ import {TIME_RANGE_START, TIME_RANGE_STOP} from 'src/variables/constants'
 // Actions & Selectors
 import {notify as notifyAction} from 'src/shared/actions/notifications'
 import {setQueryResultsByQueryID} from 'src/queryCache/actions'
-import {isInVEOMode} from 'src/shared/selectors/app'
+import {hasUpdatedTimeRangeInVEO} from 'src/shared/selectors/app'
 
 // Types
 import {
@@ -72,7 +72,7 @@ interface QueriesState {
 }
 
 interface StateProps {
-  isInVEOMode: boolean
+  hasUpdatedTimeRangeInVEO: boolean
   queryLink: string
   buckets: Bucket[]
   variables: Variable[]
@@ -314,11 +314,14 @@ class TimeSeries extends Component<Props & WithRouterProps, State> {
   }
 
   private shouldReload(prevProps: Props) {
-    if (this.props.isInVEOMode) {
+    if (this.props.hasUpdatedTimeRangeInVEO) {
       return false
     }
 
-    if (prevProps.isInVEOMode && !this.props.isInVEOMode) {
+    if (
+      prevProps.hasUpdatedTimeRangeInVEO &&
+      !this.props.hasUpdatedTimeRangeInVEO
+    ) {
       return true
     }
 
@@ -362,7 +365,7 @@ const mstp = (state: AppState, props: OwnProps): StateProps => {
   ]
 
   return {
-    isInVEOMode: isInVEOMode(state),
+    hasUpdatedTimeRangeInVEO: hasUpdatedTimeRangeInVEO(state),
     queryLink: state.links.query.self,
     buckets: getAll<Bucket>(state, ResourceType.Buckets),
     variables,
